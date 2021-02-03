12020cf3aae3bf1f0560b835766b8db5588b0783
noChar <alwaysinarush@protonmail.ch>
2021-02-03 04:13:19 -0800

    New branch for latest version.
    Staging and committing small fixes and additions such as new circle object class definition in separate file and content add/removed from main website page.

--- p5_stuff/lib/empty-example/sketch.js
+++ p5_stuff/lib/empty-example/sketch.js
@@ -1,16 +1,41 @@
+/*
+* # INFORMATION ABOUT THIS DOCUMENT
+* see README.text
+*
+*
+*
+*/
+
+/*  GLOBALS  */
+let some_circle;
+let some_other_circle;
+let name;
+let diameter;
+let x_coord;
+let y_coord;
+let mousePosX;
+let mousePosY;
+
+/*  CONSTANTS */
 
 
 
 function setup() {
   // setup code
-  createCanvas(displayWidth, displayHeight);
-
+  createCanvas(windowWidth, windowHeight);
   // Creating a new polySynth object
   polySynth = new p5.PolySynth();
 
+  diameter = random(50,100);
+  some_circle = new ACircle("Sun", 50, windowWidth/2, windowHeight/2);
+  //some_other_circle = new ACircle("Other", diameter, 100, 250);
 }
 
+//
 
+
+// Contains data about a tone.
+// Is triggered by other custom functions.
 function playSynth() {
 // What's this thing do?
   userStartAudio();
@@ -28,38 +53,41 @@ function playSynth() {
   polySynth.play('C3', vel, 0, note_dur);
 }
 
+// This function simply gets the (x,y) coordinates from the circle
+// it's passed as a parameter and checks if, given their boundaries,
+// the mouse is inside that space.
+
+// If yes, then make a sound.
+// else not, no sound.
+function checkCirclePos(some_circle){
+  // store current mouse (x,y)
+  mousePosX = mouseX();
+  mousePosY = mouseY();
+
+  // calculate coordinates of the circumference of the circle.
+
+}
+
+function makeCircleArray() {
+  // console.log("inside makeCircleArray");
+  //
+  // let circle_Array = [];
+  //
+  // for (let i=0; i<=7; i++){
+  //   circle_Array[i] = circle(50*i, 50*i, 10);
+  // }
+  // console.log(circle_Array);
+}
 
 
 function draw() {
   // put drawing code here
   background(100);
+  // console.log("inside draw");
+  //
+  // makeCircleArray();
+  // noLoop();
 
-  // Circle in the middle
-  var mid_x = displayWidth/2;
-  var mid_y = displayHeight/2;
-  circle(mid_x, mid_y, 50);
-
-  // Circle at (50, 50) that does the same thing as the one in the middle.
-  circle(50, 50, 50);
-  if(mouseX >= 25 && mouseX <= 75 && mouseY >= 25 && mouseY <= 75) {
-      // console.log("inside");
-      playSynth();
-  }
-
-  // I know the coordinates of the circle
-  //print("right_arc_center: " + (mid_x + 25) + " || top_arc_center: " + (mid_y + 25));
-
-  // I know coordinates of the mouse
-  //print(displayWidth + ", " + displayHeight);
-  console.log("mouseX: " + mouseX + "\n" + "mouseY: " + mouseY);
-
-  // Can I check when they meet?
-  if(mouseX >= (mid_x - 25) && mouseX <= (mid_x + 25) && mouseY >= (mid_y - 25) && mouseY <= (mid_y + 25)) {
-      // console.log("inside");
-      playSynth();
-  }
-
-  // Attempting circle that grows
-  circle(movedX, 100, 50);
-
+  var circle1 = circle(some_circle.getXCoord(), some_circle.getYCoord(), some_circle.getDiameter());
+  console.log(some_circle.getXCoord() + "," + some_circle.getYCoord());
 }
